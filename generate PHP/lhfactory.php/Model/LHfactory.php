1918578b9aebfaea80b3e0cea1f22f06f31f471e
Yomab <mabire.yoan@gmail.com>
2019-06-26 10:38:32 +0200

    partie1terminé

---
+++ generate PHP/lhfactory.php/Model/LHfactory.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * 
+ */
+class Employé {
+
+    /**
+     * 
+     */
+    private $Boulasse;
+
+    /**
+     * 
+     */
+    private $Roger;
+
+    /**
+     * 
+     */
+    private $1500;
+
+    /**
+     * Default constructor
+     */
+    public function __construct() {
+    }
+
+    /**
+     * 
+     */
+    public function travailler() {
+        // TODO implement here
+    }
+
+}
+/**
+ * 
+ */
+class Cadre {
+
+    /**
+     * 
+     */
+    private $avoirDesResponsabilités;
+
+    /**
+     * Default constructor
+     */
+    public function __construct() {
+    }
+
+}
